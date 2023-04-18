cc3f753ba1facb012bcee4519f272f58233d943e
CarlosPProjects <cg9822@gmail.com>
2023-04-18 20:19:22 +0200

    data object structured

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -8,8 +8,27 @@ router.get("/data", async (req: Request, res: Response) => {
   try {
     const response = await axios.get("https://precioaceitedeoliva.net/");
     const $ = cheerio.load(response.data);
-    const data = $("table").eq(0).text().replace(/\s\s+/g, " ");
-    res.send(data);
+    const tableTr = $("table").eq(0).find("tr");
+    const tableStructured = {
+      title: tableTr.eq(0).find("th").text(),
+      date: tableTr.eq(1).find("th").eq(0).text(),
+      header: tableTr.eq(1).find("th").eq(1).text(),
+      items: [
+        {
+          name: tableTr.eq(2).find("td").eq(0).text(),
+          price: tableTr.eq(2).find("td").eq(1).text(),
+        },
+        {
+          name: tableTr.eq(3).find("td").eq(0).text(),
+          price: tableTr.eq(3).find("td").eq(1).text(),
+        },
+        {
+          name: tableTr.eq(4).find("td").eq(0).text(),
+          price: tableTr.eq(4).find("td").eq(1).text(),
+        },
+      ],
+    };
+    res.send(tableStructured);
   } catch (e) {
     console.log(e);
     res.status(500).send("Error al obtener los datos");
